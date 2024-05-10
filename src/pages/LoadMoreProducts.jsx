074d8c0cb644025caeb92995b1ba5de780132cac
Bilal Benyoussef <bilalbeny735@gmail.com>
2024-05-10 12:18:46 +0200

    --no-edit

--- src/pages/LoadMoreProducts.jsx
+++ src/pages/LoadMoreProducts.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 export default function LoadMoreProducts() {
     const [products, setProducts] = useState([])
-    const [loadMore, setLoadMore] = useState(10)
+    const [count, setCount] = useState(0)
     const [isLoading, setIsLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null)
 
@@ -11,9 +11,19 @@ export default function LoadMoreProducts() {
         async function fetchData() {
             try {
                 setIsLoading(true)
-                const res = await fetch(`https://dummyjson.com/products?limit=${loadMore}`)
+                const res = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`)
                 const data = await res.json()
-                setProducts(data.products)
+                if (data && data.products.length > 0) {
+                    setProducts(count !== 0 ? prevProducts => {
+                        return {
+                            ...prevProducts,
+                            products: [
+                                ...prevProducts.products,
+                                ...data?.products
+                            ]
+                        }
+                    } : data)
+                }
                 setIsLoading(false)
             } catch (e) {
                 setErrorMessage(e.message);
@@ -22,11 +32,11 @@ export default function LoadMoreProducts() {
             }
         }
         fetchData()
-    }, [loadMore])
+    }, [count])
 
     function handleLoadMoreProductsClick() {
-        if (loadMore !== 100) {
-            setLoadMore(prevLoadMore => prevLoadMore += 30)
+        if ((count + 1) * 20 < products.total) {
+            setCount(prevCount => prevCount += 1)
         }
     }
     return (
@@ -40,7 +50,7 @@ export default function LoadMoreProducts() {
                 isLoading
                     ? <div className="grid grid-cols-3 gap-5 p-8">
                         {
-                            [...Array(loadMore)].map((_, index) => {
+                            [...Array((count + 1) * 20)].map((_, index) => {
                                 return <div key={index} className="bg-gray-100 p-4  w-full h-[300px] rounded shadow">
                                     <div className="bg-gray-500 w-full h-3/4 rounded animate-pulse"></div>
                                     <p className="w-full h-[40px] bg-gray-500 rounded mt-4 animate-pulse"></p>
@@ -50,10 +60,10 @@ export default function LoadMoreProducts() {
                     </div>
                     : <div className="grid grid-cols-3 gap-5 p-8">
                         {
-                            products.map((product) => {
+                            products?.products?.map((product) => {
                                 return <div key={product.id} className='bg-gray-100 p-4  w-full min-h-[300px] rounded shadow overflow-clip'>
                                     <div className='h-[70%] rounded mb-4'>
-                                        <img src={product.images[0]} alt={product.title} className='w-full h-full aspect-square rounded' />
+                                        <img src={product.thumbnail} alt={product.title} className='w-full h-full aspect-square rounded' />
                                     </div>
                                     <p className='text-xs'>{product.description}</p>
                                 </div>
@@ -61,7 +71,11 @@ export default function LoadMoreProducts() {
                         }
                     </div>
             }
-            <button type='button' className={`block mx-auto px-4 py-2 bg-slate-300 rounded shadow cursor-pointer text-sm font-semibold transition ${loadMore === 100 ? 'opacity-50' : 'hover:opacity-90 hover:shadow-none'}`} onClick={handleLoadMoreProductsClick}>{loadMore !== 100 ? "Load More Products" : "No more products to load"}</button>
-        </div>
+            {
+                isLoading
+                    ? <button className='block mx-auto px-5 py-2 rounded shadow bg-slate-300 animate-pulse w-[250px] h-[50px]'></button>
+                    : <button type='button' className={`block mx-auto px-4 py-2 bg-slate-300 rounded shadow cursor-pointer text-sm font-semibold transition ${((count + 1) * 20 < products.total) ? 'hover:opacity-90 hover:shadow-none' : 'opacity-50 cursor-not-allowed'}`} onClick={handleLoadMoreProductsClick}>{(count + 1) * 20 < products.total ? `Load More Products ${isLoading ? '....' : ''}` : "No more products to load"}</button>
+            }
+        </div >
     )
 }
